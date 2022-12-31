fe91e2cf1de05dcd39af2f7887fdb358ad3bb6f6
ULG23 <gauthier.miguet@gmail.com>
2022-12-31 20:21:44 +0100

    ajout liste animations

---
+++ IHM_Project/Model/Animation.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IHM_Project.Model
+{
+    [ObservableObject]
+    public partial class Animation
+    {
+        [ObservableProperty]
+        public int index;
+
+        [ObservableProperty]
+        public string? name;
+    }
+}
